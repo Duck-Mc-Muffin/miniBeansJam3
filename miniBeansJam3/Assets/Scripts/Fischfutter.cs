9a07209ffb750b59cf62a06d9f90ab63468114b7
Jona Ostermann <Jona-Ostermann@web.de>
2019-02-22 23:58:08 +0100

    Commit weil ich scheiße mergen muss

---
+++ miniBeansJam3/Assets/Scripts/Fischfutter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fischfutter : MonoBehaviour
+{
+
+    public float forceStrength;
+    
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.LookAt(new Vector3(0, transform.position.y, 0), new Vector3(0, 1, 0));
+        GetComponent<Rigidbody>().AddRelativeForce(new Vector3(forceStrength, 0, 0), ForceMode.Force);
+        GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, forceStrength), ForceMode.Force);
+    }
+}
---
+++ miniBeansJam3/Assets/Scripts/SpawnController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public Vector3 SpawnArea;
+    public GameObject FischfutterPrefab;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        GameObject fischfutter = GameObject.Instantiate(FischfutterPrefab);
+    }
+}
