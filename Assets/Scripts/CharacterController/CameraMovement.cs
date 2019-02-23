208d6f1646e9e5a4a939c337ae59400fd4989f35
Steven Pauls <stevenpauls@gmx.de>
2019-02-23 11:55:23 +0100

    CharContr. v1

---
+++ Assets/Scripts/CharacterController/CameraMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform ankerPos, ankerLookAtPos;
+    public float posLerp;
+
+    private void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, ankerPos.position, posLerp);
+        transform.LookAt(ankerLookAtPos);
+    }
+}
