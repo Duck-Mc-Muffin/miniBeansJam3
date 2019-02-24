a929da306d5ec00ecbad1c48d4554173545a8d35
Steven Pauls <stevenpauls@gmx.de>
2019-02-24 17:21:02 +0100

    player score time

--- Assets/Scripts/GameLogik.cs
+++ Assets/Scripts/GameLogik.cs
@@ -2,20 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameLogik : MonoBehaviour
 {
     public static GameLogik instance; 
 
-    public float maxTimeToLive;
+    public float maxTimeToLive, gameOverPause;
     public string foodTag = "Food";
     public float foodTimeBonus = 10f;
     public ParticleSystem bubbleBurst;
 
-    public Text txt_TTL;
+    public Text txt_Score;
     public Slider sld_TTL;
+    public int sceneIndex_DeathScreen;
 
-    private float timeToLive;
+    private float timeToLive, currentScoreTime, currentGameOverTime;
     private Rigidbody phy;
 
     public float TimeToLive
@@ -24,10 +26,26 @@ public class GameLogik : MonoBehaviour
         set
         {
             timeToLive = value;
-            if (timeToLive <= 0) print("Player died!");
-            txt_TTL.text = timeToLive > maxTimeToLive ? maxTimeToLive.ToString() : timeToLive.ToString("N2").Replace(',', '.'); // TODO: Formatierung ändern.
-            txt_TTL.text += " sec.";
+            if (timeToLive <= 0)
+            {
+                timeToLive = 0;
+                CharacterMovement.instance.enabled = false;
+                PlayerPrefs.SetString("LastTime", txt_Score.text);
+                print(PlayerPrefs.GetString("LastTime"));
+            }
             sld_TTL.value = timeToLive > maxTimeToLive ? 1 : timeToLive / maxTimeToLive;
+        }
+    }
+
+    public float ScoreTime
+    {
+        get { return currentScoreTime; }
+        set
+        {
+            currentScoreTime = value;
+            int minutes = Mathf.FloorToInt(currentScoreTime) / 60;
+            float seconds = (currentScoreTime - minutes * 60);
+            txt_Score.text = minutes + ":" + (seconds < 10 ? "0" + seconds.ToString("N2") : seconds.ToString("N2")).Replace(',', '.'); // TODO: Formatierung ändern.
         }
     }
 
@@ -40,18 +58,28 @@ public class GameLogik : MonoBehaviour
     {
         TimeToLive = maxTimeToLive;
         phy = GetComponent<Rigidbody>();
+        ScoreTime = 0;
     }
 
     private void Update()
     {
-        TimeToLive -= Time.deltaTime;
+        if (TimeToLive > 0)
+        {
+            ScoreTime += Time.deltaTime;
+            TimeToLive -= Time.deltaTime;
+        }
+        else
+        {
+            currentGameOverTime += Time.deltaTime;
+            if (currentGameOverTime > gameOverPause) SceneManager.LoadScene(sceneIndex_DeathScreen);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.tag == foodTag)
         {
-            if (TimeToLive > maxTimeToLive) return;
+            if (TimeToLive > maxTimeToLive || TimeToLive <= 0) return;
             TimeToLive += foodTimeBonus;
             Destroy(collision.gameObject);
             bubbleBurst.transform.position = collision.contacts[0].point;
